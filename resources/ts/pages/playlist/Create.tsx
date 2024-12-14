207db7c10fbe864e1677c2ffd20b012c4397361f
Kyamada2001 <yamakai173git@gmail.com>
2024-12-14 12:29:40 +0900

    フォルダアップロードUIを追加

--- resources/ts/pages/playlist/Create.tsx
+++ resources/ts/pages/playlist/Create.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
+// TODO: validate
+// TODO: アップしたフォルダ、ファイルを表示する機能
 const PlayListCreate = () => {
+    const directoryRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (directoryRef.current !== null) {
+            directoryRef.current.setAttribute("directory", "");
+            directoryRef.current.setAttribute("webkitdirectory", "");
+        }
+    }, [directoryRef]);
+
+    const handleFileChange = (files: FileList | null) => {
+        console.log(files);
+    }
+
     return (
-        <h1>create</h1>
+        <input
+            accept=""
+            multiple
+            type="file"
+
+            ref={directoryRef}
+            onChange={(e) => { handleFileChange(e.currentTarget.files); }}
+        />
     )
 }
 

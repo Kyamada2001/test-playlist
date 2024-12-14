6b05d013ca54e6890dca678f6dbe6f2c41bf7877
Kyamada2001 <yamakai173git@gmail.com>
2024-12-14 11:44:04 +0900

    laravel側のrouteを、如何なるアクセスもvite側に渡すように

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('index');
-});
+})->where('any', '.*');
